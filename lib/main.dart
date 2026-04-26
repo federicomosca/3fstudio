6cce8937a32d2ac741275416e78f53f14a876d93
federicomosca <federicomosca@pm.me>
2026-04-26 11:23:57 +0200

    sentry added

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'core/config/supabase_config.dart';
 import 'core/providers/theme_provider.dart';
 import 'core/router/app_router.dart';
 import 'core/theme/app_theme.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,7 +19,23 @@ void main() async {
     anonKey: SupabaseConfig.anonKey,
   );
 
-  runApp(const ProviderScope(child: App3FStudio()));
+  await SentryFlutter.init(
+    (options) {
+      options.dsn =
+          'https://a2c2696c35f25ff0f7e8b0197712a92f@o4511285544026112.ingest.de.sentry.io/4511285546975312';
+      // Adds request headers and IP for users, for more info visit:
+      // https://docs.sentry.io/platforms/dart/guides/flutter/data-management/data-collected/
+      options.sendDefaultPii = true;
+      // Set tracesSampleRate to 1.0 to capture 100% of transactions for tracing.
+      // We recommend adjusting this value in production.
+      options.tracesSampleRate = 1.0;
+      // The sampling rate for profiling is relative to tracesSampleRate
+      // Setting to 1.0 will profile 100% of sampled transactions:
+      options.profilesSampleRate = 1.0;
+    },
+    appRunner: () =>
+        runApp(SentryWidget(child: const ProviderScope(child: App3FStudio()))),
+  );
 }
 
 class App3FStudio extends ConsumerWidget {
